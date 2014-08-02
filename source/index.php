31675e65a309213820aadc51a5f14cc1318cd42a
Iago Melanias <me@imelanias.com>
2014-08-02 06:30:57 -0300

    Adding status file

--- source/index.php
+++ source/index.php
@@ -8,6 +8,11 @@ if($_SERVER["HTTP_X_PJAX"]):
 	<li><a href='trex.php' data-pjax='main'>Tyrannosaurus</a></li>
 	<li><a href='kong.php' data-pjax='main' >King Kong</a></li>
 </nav>
+<?php
+$imhome = true;
+include("status.php");
+echo $what;
+?>
 
 <?php else: 
 	include("header.php");
@@ -20,6 +25,12 @@ if($_SERVER["HTTP_X_PJAX"]):
 			<li><a href='trex.php' data-pjax='main'>Tyrannosaurus</a></li>
 			<li><a href='kong.php' data-pjax='main' >King Kong</a></li>
 		</nav>
+
+		<?php 
+		$imhome = true;
+		include("status.php");
+		echo $what;
+		?>
 	</section>
 <?php
 include("footer.php");	
