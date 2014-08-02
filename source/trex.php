4c30ac4fec9470f6d9fd47c30fc037fd6aad20b9
Iago Melanias <me@imelanias.com>
2014-08-02 06:10:24 -0300

    Ordering kong and trex pages better

--- source/trex.php
+++ source/trex.php
@@ -2,14 +2,19 @@
 $title = 'Tyrannosaurus';
 $body = "<ul class='header'><li><a href='index.php' data-pjax='main'>back</a></li></ul><h2>$title</h2><img src='images/Trex.jpg' />";
 
- if($_SERVER["HTTP_X_PJAX"]): 
-	echo $body;
-	echo "<title>{$title}</title>";
+if($_SERVER["HTTP_X_PJAX"]): 
+
+// If the page is being loaded using Pjax
+echo $body;
+echo "<title>{$title}</title>";
 
 else: 
+
+// If not
 include("header.php");
-	echo '<div id="main">'.$body.'</div>';
+echo '<div id="main">'.$body.'</div>';
 include("footer.php");	
+
 endif;
 
 echo "<pre>";
